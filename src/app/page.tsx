6c119be3b879f195db22e09380ab2318a5cb8c23
ramazan <ramazan.abdullaev.00@gmail.com>
2024-11-02 18:11:47 +0300

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const router = useRouter();
 
   const onMouseEnterHandler = (target: EventTarget) => {
-    const videoSrc = target.dataset.videoSrc;
+    const videoSrc = (target as HTMLElement).dataset.videoSrc;
 
     setBackgroundVideo(videoSrc);
   };
