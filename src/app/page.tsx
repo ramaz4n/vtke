49f5a0d46d1262aa2cb176f8893ac7866a5b44d7
ramazan <ramazan.abdullaev.00@gmail.com>
2024-11-09 19:55:49 +0300

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,8 +44,124 @@ export default function Home() {
     }
   }, [nextVideo]);
 
+  const mainRouteItems = [
+    {
+      href: '/about',
+      id: 1,
+      routeList: [
+        {
+          href: '/about',
+          id: 1,
+          text: 'О нас'
+        },
+        {
+          href: '/about',
+          id: 2,
+          text: 'Видео'
+        },
+        {
+          href: '/about',
+          id: 3,
+          text: 'Команда'
+        },
+        {
+          href: '/about',
+          id: 4,
+          text: 'Цели'
+        },
+      ],
+      title: 'Компания',
+      videoSrc: 'videos/1.mp4'
+    },
+    {
+      href: '/products',
+      id: 2,
+      routeList: [
+        {
+          href: '/about',
+          id: 1,
+          text: 'О нас'
+        },
+        {
+          href: '/about',
+          id: 2,
+          text: 'Видео'
+        },
+        {
+          href: '/about',
+          id: 3,
+          text: 'Команда'
+        },
+        {
+          href: '/about',
+          id: 4,
+          text: 'Цели'
+        },
+      ],
+      title: 'Продукты',
+      videoSrc: 'videos/2.mp4'
+    },
+    {
+      href: '/services',
+      id: 3,
+      routeList: [
+        {
+          href: '/about',
+          id: 1,
+          text: 'О нас'
+        },
+        {
+          href: '/about',
+          id: 2,
+          text: 'Видео'
+        },
+        {
+          href: '/about',
+          id: 3,
+          text: 'Команда'
+        },
+        {
+          href: '/about',
+          id: 4,
+          text: 'Цели'
+        },
+      ],
+      title: 'Услуги',
+      videoSrc: 'videos/3.mp4'
+    },
+    {
+      href: '/news',
+      id: 4,
+      routeList: [
+        {
+          href: '/about',
+          id: 1,
+          text: 'О нас'
+        },
+        {
+          href: '/about',
+          id: 2,
+          text: 'Видео'
+        },
+        {
+          href: '/about',
+          id: 3,
+          text: 'Команда'
+        },
+        {
+          href: '/about',
+          id: 4,
+          text: 'Цели'
+        },
+      ],
+      title: 'Новости',
+      videoSrc: 'videos/4.mp4'
+    },
+  ];
+
   return (
     <>
+      <div className="absolute left-0 top-0 -z-10 size-full bg-black"></div>
       <video ref={currentVideoRef} autoPlay loop muted
         className={cc([{ 'opacity-0 -z-1': isFadingOut }, 'absolute left-0 top-0 z-0 size-full object-cover transition-opacity duration-200 ease'])}>
         <source src={currentVideo} type="video/mp4" />
@@ -55,70 +171,29 @@ export default function Home() {
         <source src={nextVideo} type="video/mp4" />
       </video>
       <div className="fixed flex size-full">
-        <div
-          className="group flex h-full w-1/4 cursor-pointer items-center justify-center border-x-2 border-transparent hover:border-white"
-          data-video-src="videos/1.mp4"
-          onClick={() => router.push('/about')}
-          onMouseEnter={(event) => onMouseEnterHandler(event.target)}
-        >
-          <div className="relative">
-            <h2 className="text-center text-5xl font-bold text-white">Компания</h2>
-            <div className="invisible absolute left-0 top-24 ml-1 flex flex-col gap-3 text-white group-hover:visible">
-              <Link className="text-xl hover:underline" href="/about">О нас</Link>
-              <Link className="text-xl hover:underline" href="/about">Видео</Link>
-              <Link className="text-xl hover:underline" href="/about">Команда</Link>
-              <Link className="text-xl hover:underline" href="/about">Цели</Link>
+        {
+          mainRouteItems.map((item, index) => (
+            <div
+              key={index}
+              className="group flex h-full w-1/4 cursor-pointer items-center justify-center border-x-2 border-transparent hover:border-white"
+              data-video-src={item.videoSrc}
+              onClick={() => router.push(item.href)}
+              onMouseEnter={(event) => onMouseEnterHandler(event.target)}
+            >
+              <div className="relative">
+                <h2 className="text-center text-5xl font-bold text-white"> {item.title}</h2>
+                <div
+                  className="invisible absolute left-0 top-24 ml-1 flex flex-col gap-3 text-white group-hover:visible">
+                  {
+                    item.routeList.map((route, index) => (
+                      <Link key={index} className="text-xl hover:underline" href={route.href}>{route.text}</Link>
+                    ))
+                  }
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div
-          className="group flex h-full w-1/4 cursor-pointer items-center justify-center border-x-2 border-transparent hover:border-white"
-          data-video-src="videos/2.mp4"
-          onClick={() => router.push('/products')}
-          onMouseEnter={(event) => onMouseEnterHandler(event.target)}
-        >
-          <div className="relative">
-            <h2 className="text-center text-5xl font-bold text-white">Продукты</h2>
-            <div className="invisible absolute left-0 top-24 ml-1 flex flex-col gap-3 text-white group-hover:visible">
-              <Link className="text-xl hover:underline" href="/about">О нас</Link>
-              <Link className="text-xl hover:underline" href="/about">Видео</Link>
-              <Link className="text-xl hover:underline" href="/about">Команда</Link>
-              <Link className="text-xl hover:underline" href="/about">Цели</Link>
-            </div>
-          </div>
-        </div>
-        <div
-          className="group flex h-full w-1/4 cursor-pointer items-center justify-center border-x-2 border-transparent hover:border-white"
-          data-video-src="videos/3.mp4"
-          onClick={() => router.push('/services')}
-          onMouseEnter={(event) => onMouseEnterHandler(event.target)}
-        >
-          <div className="relative">
-            <h2 className="text-center text-5xl font-bold text-white">Услуги</h2>
-            <div className="invisible absolute left-0 top-24 ml-1 flex flex-col gap-3 text-white group-hover:visible">
-              <Link className="text-xl hover:underline" href="/about">О нас</Link>
-              <Link className="text-xl hover:underline" href="/about">Видео</Link>
-              <Link className="text-xl hover:underline" href="/about">Команда</Link>
-              <Link className="text-xl hover:underline" href="/about">Цели</Link>
-            </div>
-          </div>
-        </div>
-        <div
-          className="group flex h-full w-1/4 cursor-pointer items-center justify-center border-x-2 border-transparent hover:border-white"
-          data-video-src="videos/4.mp4"
-          onClick={() => router.push('/news')}
-          onMouseEnter={(event) => onMouseEnterHandler(event.target)}
-        >
-          <div className="relative">
-            <h2 className="text-center text-5xl font-bold text-white">Новости</h2>
-            <div className="invisible absolute left-0 top-24 ml-1 flex flex-col gap-3 text-white group-hover:visible">
-              <Link className="text-xl hover:underline" href="/about">О нас</Link>
-              <Link className="text-xl hover:underline" href="/about">Видео</Link>
-              <Link className="text-xl hover:underline" href="/about">Команда</Link>
-              <Link className="text-xl hover:underline" href="/about">Цели</Link>
-            </div>
-          </div>
-        </div>
+          ))
+        }
       </div>
     </>
   );
