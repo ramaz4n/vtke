2260b40f1761335bfbcdc25b5fa56b840ce4c267
ramazan <ramazan.abdullaev.00@gmail.com>
2025-02-04 23:01:22 +0300

    [ramazan] - fix eslint errors

--- src/shared/utils/maskito.ts
+++ src/shared/utils/maskito.ts
@@ -1,8 +1,8 @@
-import { Masks } from '@/shared/constants/masks.ts';
+import { MASKS } from '@/shared/constants/masks.ts';
 import formatWithMask from '@/shared/utils/format-with-mask.ts';
 
 export class Maskito {
-  format(text?: string, mask = Masks.phone): string {
+  format(text?: string, mask = MASKS.phone): string {
     return formatWithMask({ mask, text }).masked;
   }
 }
